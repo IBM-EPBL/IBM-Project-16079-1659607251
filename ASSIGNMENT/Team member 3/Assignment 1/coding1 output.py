379df51b7ccceada833afc5d3a6718c9df1350b9
Vimalamadesh <113982762+Vimalamadesh@users.noreply.github.com>
2022-09-23 16:03:03 +0530

    Add files via upload

---
+++ ASSIGNMENT/Team member 3/Assignment 1/coding1 output.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:\Users\rajas\Downloads\coding 1.py ================
+[20, 10, 23, 30, 40]
+[20, 10, 20, 23, 30, 40]
+[10, 20, 23, 30, 40]
+[10, 20, 23, 30, 40, -2]
+[-2, 10, 20, 23, 30, 40]
+[-2, 10, 20, 23, 30]
+[30, 23, 20, 10, -2]
+HiBuddy
+tnemnorivnE
+ni
